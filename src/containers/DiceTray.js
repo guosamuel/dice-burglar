c7e5987cbe42318023f326b394319ef9cc1e657f
Samuel Guo <47992471+guosamuel@users.noreply.github.com>
2019-09-25 15:26:42 -0400

    render Die Square in Dice Tray

--- src/containers/DiceTray.js
+++ src/containers/DiceTray.js
@@ -1,8 +1,18 @@
-import React from 'react'
+// import React from 'react'
 import Die from '../components/Die'
 import DieSquare from '../components/DieSquare'
 
-class DiceTray extends React.Component {
+function renderDieSquare(x, y, [dieX, dieY]) {
+  const x = i % 2
+  const y = Math.floor(i/2)
+  const isDieHere = x === dieX && y === dieY
+  const red = (x + y) % 2 === 1
+  const piece = isDieHere ? <Die /> : null
+
+  return <DieSquare red={red}>{piece}</DieSqaure>
+}
+
+function DiceTray  {
   render() {
     return (
       <div>
