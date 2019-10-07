13c2203635f2a851ac53027e0c5ab2c307fa63a6
Samuel Guo <47992471+guosamuel@users.noreply.github.com>
2019-10-07 16:45:48 -0400

    made the die draggable but only works for center dice tray

--- src/components/DiceTraySquare.js
+++ src/components/DiceTraySquare.js
@@ -1,7 +1,47 @@
 import React from 'react'
 import DieSquare from './DieSquare'
+import { ItemTypes } from '../constants/constants'
+import { moveDie } from '../observations/observe'
+import { useDrop } from 'react-dnd'
 
 export default function DiceTraySquare({x, y, children}) {
   const red = (x + y) % 2 === 1
-  return <DieSquare red={red}>{children}</DieSquare>  
+
+  const [{ isOver, canDrop }, drop] = useDrop({
+    accept: ItemTypes.DIE,
+    drop: () => moveDie(x, y),
+    collect: mon => ({
+      isOver: !!mon.isOver(),
+      canDrop: !!mon.canDrop(),
+    }),
+  })
+
+  return (
+    <div
+      ref={drop}
+      style={{
+        position: 'relative',
+        width: '100%',
+        height: '100%'
+      }}
+    >
+      <DieSquare red={red}>{children}</DieSquare>
+      {isOver &&
+        (
+          <div
+            style={{
+              position: 'absolute',
+              top: 0,
+              left: 0,
+              height: '100%',
+              width: '100%',
+              zIndex: 1,
+              opacity: 0.5,
+              backgroundColor: 'yellow',
+            }}
+          />
+        )
+      }
+    </div>
+  )
 }
