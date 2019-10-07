feaaaad4780ec3d79e57e9ab6adf06f4b0b5cd96
Samuel Guo <47992471+guosamuel@users.noreply.github.com>
2019-10-07 13:59:26 -0400

    wrote out observation functions for clicking on different squares

--- src/observations/observe.js
+++ src/observations/observe.js
@@ -1,4 +1,25 @@
-export function observe(receive) {
-  const randPos = () => Math.floor(Math.random() * 4)
-  setInterval(() => receive([randPos(), randPos()]), 500)
+// export function observe(receive) {
+//   const randPos = () => Math.floor(Math.random() * 4)
+//   setInterval(() => receive([randPos(), randPos()]), 2000)
+// }
+
+let diePosition = [1, 1]
+let observer = null
+
+function emitChange() {
+  observer(diePosition)
+}
+
+export function observe(o) {
+  if (observer) {
+    throw new Error('Multiple observers not implemented.')
+  }
+
+  observer = o
+  emitChange()
+}
+
+export function moveDie(toX, toY) {
+  diePosition = [toX, toY]
+  emitChange()
 }
