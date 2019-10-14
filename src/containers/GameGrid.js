15db30a08b517c0f535efb6c31c8038319911b2a
Samuel Guo <47992471+guosamuel@users.noreply.github.com>
2019-10-14 15:27:52 -0400

    converted the GameGrid from a class to a function in order to implement hooks

--- src/containers/GameGrid.js
+++ src/containers/GameGrid.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Grid, Image } from 'semantic-ui-react'
 import BurglarContainer from '../containers/BurglarContainer'
 import DicePool from '../components/DicePool.js'
@@ -6,52 +6,51 @@ import DicePool from '../components/DicePool.js'
 import { DndProvider } from 'react-dnd'
 import HTML5Backend from 'react-dnd-html5-backend'
 
-class GameGrid extends React.Component {
-  render() {
-    return (
-      <div>
-        <DndProvider backend={HTML5Backend}>
-          <Grid columns={3} celled={true} style={{height: '100vh'}}>
-            <Grid.Row style={{height: '33%'}}>
-              <Grid.Column verticalAlign='center'>
-                <BurglarContainer observingDiePosition={this.props.observingDiePosition}/>
-              </Grid.Column>
-              <Grid.Column verticalAlign='center'>
-                <BurglarContainer observingDiePosition={this.props.observingDiePosition}/>
-              </Grid.Column>
-              <Grid.Column verticalAlign='center'>
-                <BurglarContainer observingDiePosition={this.props.observingDiePosition}/>
-              </Grid.Column>
-            </Grid.Row>
+function GameGrid(props) {
+  return (
+    <div>
+      <DndProvider backend={HTML5Backend}>
+        <Grid columns={3} celled={true} style={{height: '100vh'}}>
+          <Grid.Row style={{height: '33%'}}>
+            <Grid.Column verticalAlign='center'>
+              <BurglarContainer observingDiePosition={props.observingDiePosition}/>
+            </Grid.Column>
+            <Grid.Column verticalAlign='center'>
+              <BurglarContainer observingDiePosition={props.observingDiePosition}/>
+            </Grid.Column>
+            <Grid.Column verticalAlign='center'>
+              <BurglarContainer observingDiePosition={props.observingDiePosition}/>
+            </Grid.Column>
+          </Grid.Row>
 
-            <Grid.Row style={{height: '33%'}}>
-              <Grid.Column verticalAlign='center'>
-                <BurglarContainer observingDiePosition={this.props.observingDiePosition}/>
-              </Grid.Column>
-              <Grid.Column verticalAlign='center'>
-                <DicePool observingDiePosition={this.props.observingDiePosition}/>
-              </Grid.Column>
-              <Grid.Column verticalAlign='center'>
-                <BurglarContainer observingDiePosition={this.props.observingDiePosition}/>
-              </Grid.Column>
-            </Grid.Row>
+          <Grid.Row style={{height: '33%'}}>
+            <Grid.Column verticalAlign='center'>
+              <BurglarContainer observingDiePosition={props.observingDiePosition}/>
+            </Grid.Column>
+            <Grid.Column verticalAlign='center'>
+              <DicePool observingDiePosition={props.observingDiePosition}/>
+            </Grid.Column>
+            <Grid.Column verticalAlign='center'>
+              <BurglarContainer observingDiePosition={props.observingDiePosition}/>
+            </Grid.Column>
+          </Grid.Row>
 
-            <Grid.Row style={{height: '33%'}}>
-              <Grid.Column verticalAlign='center'>
-                <BurglarContainer observingDiePosition={this.props.observingDiePosition}/>
-              </Grid.Column>
-              <Grid.Column verticalAlign='center'>
-                <BurglarContainer observingDiePosition={this.props.observingDiePosition}/>
-              </Grid.Column>
-              <Grid.Column verticalAlign='center'>
-                <BurglarContainer observingDiePosition={this.props.observingDiePosition}/>
-              </Grid.Column>
-            </Grid.Row>
-          </Grid>
-        </DndProvider>
-      </div>
-    )
-  }
+          <Grid.Row style={{height: '33%'}}>
+            <Grid.Column verticalAlign='center'>
+              <BurglarContainer observingDiePosition={props.observingDiePosition}/>
+            </Grid.Column>
+            <Grid.Column verticalAlign='center'>
+              <BurglarContainer observingDiePosition={props.observingDiePosition}/>
+            </Grid.Column>
+            <Grid.Column verticalAlign='center'>
+              <BurglarContainer observingDiePosition={props.observingDiePosition}/>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </DndProvider>
+    </div>
+  )
 }
+
 
 export default GameGrid
