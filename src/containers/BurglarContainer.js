47c65c47475c2968d0402d6fc5a354986a92a125
Samuel Guo <47992471+guosamuel@users.noreply.github.com>
2019-10-14 16:22:03 -0400

    was able to assign unique values to the burglar containers

--- src/containers/BurglarContainer.js
+++ src/containers/BurglarContainer.js
@@ -1,28 +1,31 @@
-import React from 'react'
+//BURGLAR CONTAINER WAS MOVED INTO THE GAME GRID IN ORDER TO USE THE
+//USE CONTEXT HOOK
+
+import React, { useContext } from 'react'
 import { Grid } from 'semantic-ui-react'
 import DiceTray from './DiceTray'
 
-class BurglarContainer extends React.Component {
-  render() {
-    return (
-      <div>
-        <Grid textAlign='center' columns={1} celled={true} style={{height: '25vh'}}>
-          <Grid.Row style={{height: '10%'}}>
-            Burglar Name
-          </Grid.Row>
-          <Grid.Row style={{height: '25%'}}>
-            Score: 0/20
-          </Grid.Row>
-          <Grid.Row style={{height: '10%'}}>
-            Current Dice
-          </Grid.Row>
-          <Grid.Row style={{height: '55%'}}>
-            <DiceTray diePosition={this.props.observingDiePosition} />
-          </Grid.Row>
-        </Grid>
-      </div>
-    )
-  }
+function BurglarContainer(props) {
+  const burglarContainerID = useContext(BurglarContainerIDs)
+  return (
+    <div>
+      <Grid textAlign='center' columns={1} celled={true} style={{height: '25vh'}}>
+        <Grid.Row style={{height: '10%'}}>
+          Burglar Name
+        </Grid.Row>
+        <Grid.Row style={{height: '25%'}}>
+          Score: 0/20
+        </Grid.Row>
+        <Grid.Row style={{height: '10%'}}>
+          Current Dice
+        </Grid.Row>
+        <Grid.Row style={{height: '55%'}}>
+          <DiceTray diePosition={props.observingDiePosition} />
+        </Grid.Row>
+      </Grid>
+    </div>
+  )
 }
+
 
 export default BurglarContainer
--- src/containers/GameGrid.js
+++ src/containers/GameGrid.js
@@ -1,10 +1,13 @@
 import React, { useContext } from 'react'
 import { Grid, Image } from 'semantic-ui-react'
-import BurglarContainer from '../containers/BurglarContainer'
+// import BurglarContainer from '../containers/BurglarContainer'
 import DicePool from '../components/DicePool.js'
+import DiceTray from './DiceTray'
 
 import { DndProvider } from 'react-dnd'
 import HTML5Backend from 'react-dnd-html5-backend'
+
+const BurglarContainerIDs = React.createContext()
 
 function GameGrid(props) {
   return (
@@ -13,37 +16,51 @@ function GameGrid(props) {
         <Grid columns={3} celled={true} style={{height: '100vh'}}>
           <Grid.Row style={{height: '33%'}}>
             <Grid.Column verticalAlign='center'>
-              <BurglarContainer observingDiePosition={props.observingDiePosition}/>
+              <BurglarContainerIDs.Provider value={1}>
+                <BurglarContainer observingDiePosition={props.observingDiePosition} />
+              </BurglarContainerIDs.Provider>
             </Grid.Column>
             <Grid.Column verticalAlign='center'>
-              <BurglarContainer observingDiePosition={props.observingDiePosition}/>
+              <BurglarContainerIDs.Provider value={2}>
+                <BurglarContainer observingDiePosition={props.observingDiePosition}/>
+              </BurglarContainerIDs.Provider>
             </Grid.Column>
             <Grid.Column verticalAlign='center'>
-              <BurglarContainer observingDiePosition={props.observingDiePosition}/>
+              <BurglarContainerIDs.Provider value={3}>
+                <BurglarContainer observingDiePosition={props.observingDiePosition}/>
+              </BurglarContainerIDs.Provider>
             </Grid.Column>
           </Grid.Row>
 
           <Grid.Row style={{height: '33%'}}>
             <Grid.Column verticalAlign='center'>
-              <BurglarContainer observingDiePosition={props.observingDiePosition}/>
+              <BurglarContainerIDs.Provider value={4}>
+                <BurglarContainer observingDiePosition={props.observingDiePosition}/>
+              </BurglarContainerIDs.Provider>
             </Grid.Column>
             <Grid.Column verticalAlign='center'>
               <DicePool observingDiePosition={props.observingDiePosition}/>
             </Grid.Column>
-            <Grid.Column verticalAlign='center'>
-              <BurglarContainer observingDiePosition={props.observingDiePosition}/>
-            </Grid.Column>
+              <BurglarContainerIDs.Provider value={6}>
+                <BurglarContainer observingDiePosition={props.observingDiePosition}/>
+              </BurglarContainerIDs.Provider>
           </Grid.Row>
 
           <Grid.Row style={{height: '33%'}}>
             <Grid.Column verticalAlign='center'>
-              <BurglarContainer observingDiePosition={props.observingDiePosition}/>
+              <BurglarContainerIDs.Provider value={7}>
+                <BurglarContainer observingDiePosition={props.observingDiePosition}/>
+              </BurglarContainerIDs.Provider>
             </Grid.Column>
+              <Grid.Column verticalAlign='center'>
+                <BurglarContainerIDs.Provider value={8}>
+                  <BurglarContainer observingDiePosition={props.observingDiePosition}/>
+                </BurglarContainerIDs.Provider>
+              </Grid.Column>
             <Grid.Column verticalAlign='center'>
-              <BurglarContainer observingDiePosition={props.observingDiePosition}/>
-            </Grid.Column>
-            <Grid.Column verticalAlign='center'>
-              <BurglarContainer observingDiePosition={props.observingDiePosition}/>
+              <BurglarContainerIDs.Provider value={9}>
+                <BurglarContainer observingDiePosition={props.observingDiePosition}/>
+              </BurglarContainerIDs.Provider>
             </Grid.Column>
           </Grid.Row>
         </Grid>
@@ -52,5 +69,27 @@ function GameGrid(props) {
   )
 }
 
+function BurglarContainer(props) {
+  const burglarContainerID = useContext(BurglarContainerIDs)
+  console.log(burglarContainerID)
+  return (
+    <div>
+      <Grid textAlign='center' columns={1} celled={true} style={{height: '25vh'}}>
+        <Grid.Row style={{height: '10%'}}>
+          Burglar Name
+        </Grid.Row>
+        <Grid.Row style={{height: '25%'}}>
+          Score: 0/20
+        </Grid.Row>
+        <Grid.Row style={{height: '10%'}}>
+          Current Dice
+        </Grid.Row>
+        <Grid.Row style={{height: '55%'}}>
+          <DiceTray diePosition={props.observingDiePosition} />
+        </Grid.Row>
+      </Grid>
+    </div>
+  )
+}
 
 export default GameGrid
